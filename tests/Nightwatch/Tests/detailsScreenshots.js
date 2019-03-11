0a994893223c015d86fcd72e0f2ce27060afca8f
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-03-11 15:21:47 +0100

    Initial commit

---
+++ tests/Nightwatch/Tests/detailsScreenshots.js
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * Captures Details with different states.
+ */
+module.exports = {
+  '@tags': ['claro'],
+  before(browser) {
+    if (browser.drupalInstall) {
+      browser.drupalInstall({
+        installProfile: 'clarodist'
+      });
+    }
+  },
+  after(browser) {
+    if (browser.drupalUninstall) {
+      browser.drupalUninstall().end();
+    }
+    else {
+      browser.end();
+    }
+  },
+  'Details'(browser) {
+    ['', 'he'].forEach((langprefix) => {
+      browser
+        .resizeWindow(1024, 600)
+        .smartURL((langprefix ? '/' + langprefix : '') + '/admin/config/system/site-information')
+        .waitForElementPresent('#edit-site-information[open] > summary')
+        .savefullScreenShot('01', langprefix, 'Details default')
+        // Collapse site info details and leave focus there.
+        .click('#edit-site-information[open] > summary')
+        .waitTillElementPresent('#edit-site-information:not([open]) > summary')
+        .focusOn('#edit-site-information:not([open]) > summary') // For Firefox.
+        .pause(100)
+        .saveScreenShot('02', langprefix, 'Details closed')
+        // Expand it again.
+        .click('#edit-site-information:not([open]) > summary')
+        .waitTillElementPresent('#edit-site-information[open] > summary')
+        .focusOn('#edit-site-information[open] > summary') // For Firefox.
+        .pause(100)
+        .saveScreenShot('03', langprefix, 'Details expanded again');
+    });
+  }
+};
